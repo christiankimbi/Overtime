6e704d9680b27fcd1b6f52d9e725fc9fd29a6fe0
Christian N. Kimbi <christiankimbi@gmail.com>
2018-07-19 01:13:54 +0200

    Added TDD for creating a new Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
   def index
 
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rationale))
+    @post.save
+    redirect_to @post
+  end
 end
