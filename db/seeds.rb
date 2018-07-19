6b733712828aad39bad11fd389582233728805ea
Christian N. Kimbi <christiankimbi@gmail.com>
2018-07-19 05:05:55 +0200

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+
+@user =  User.create(email: "chris@dren.co.za", password: "kringle1$", password_confirmation: "kringle1$", first_name: "Christian", last_name: "Kimbi")
+puts "User created"
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "rationale content no:  #{post}")
+  Post.create(date: Date.today, rationale: "rationale content no:  #{post}", user_id:  @user.id)
 end
 
 puts "100 post have been created"
