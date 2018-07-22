2f422ca5ebdec1ebda300c5367eca310836150eb
Christian N. Kimbi <christiankimbi@gmail.com>
2018-07-22 04:45:41 +0200

    Added status to Posts

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -14,6 +14,8 @@ class PostDashboard < Administrate::BaseDashboard
     rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::EnumField.with_options(searchable: false),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -23,15 +25,16 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
-    :id,
     :date,
     :rationale,
+    :status,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
+    :status,
     :id,
     :date,
     :rationale,
@@ -46,6 +49,7 @@ class PostDashboard < Administrate::BaseDashboard
     :user,
     :date,
     :rationale,
+    :status,
   ].freeze
 
   # Overwrite this method to customize how posts are displayed
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 
 @user =  User.create(email: "chris@dren.co.za", password: "kringle1$", password_confirmation: "kringle1$", first_name: "Christian", last_name: "Kimbi")
 puts "User created"
+@admin_user =  AdminUser.create(email: "info@dren.co.za", password: "kringle1$", password_confirmation: "kringle1$", first_name: "Dren", last_name: "Solutions")
+puts "Created Admin"
 
 100.times do |post|
   Post.create(date: Date.today, rationale: "rationale content no:  #{post}", user_id:  @user.id)
