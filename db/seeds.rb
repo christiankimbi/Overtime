5e657cdb4053a452479e61fcc758a275556b0151
Christian N. Kimbi <christiankimbi@gmail.com>
2018-08-20 05:07:57 +0200

    Modified seed and added images for favicons

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,15 +16,14 @@ puts "Created Admin"
 puts "User created"
 
 
+AuditLog.create(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create(user_id: User.last.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create(user_id: User.last.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 Audit logs created"
+
 100.times do |post|
   Post.create(date: Date.today, rationale: "rationale content no:  #{post}", user_id:  @user.id, overtime_request: 2.5)
 end
 
 puts "100 post have been created"
-
-
-  AuditLog.create(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
-  AuditLog.create(user_id: User.last.id, status: 0, start_date: (Date.today - 13.days))
-  AuditLog.create(user_id: User.last.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "100 Audit logs created"
