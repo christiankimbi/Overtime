5aae680e07cfaeeb8a45416f1b8695a1cd72f172
Christian N. Kimbi <christiankimbi@gmail.com>
2018-08-20 22:22:54 +0200

    Testing env from Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,16 +105,8 @@ Rails.application.configure do
   #    openssl_verify_mode: "none",
   #    enable_starttls_auto: true }
 
-  #config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
 
-  ActionMailer::Base.smtp_settings ={
-      :port =>              ENV['SPARKPOST_SMTP_PORT'],
-      :address =>           ENV['SPARKPOST_SMTP_HOST'],
-      :user_name =>           ENV['SPARKPOST_SMTP_USERNAME'],
-      :password =>           ENV['SPARKPOST_SMTP_PASSWORD'],
-      :authentication => :login,
-      :enable_starttls_auto => true
-  }
-  ActionMailer::Base.delivery_method = :smtp
+
   config.action_mailer.raise_delivery_errors = true
 end
