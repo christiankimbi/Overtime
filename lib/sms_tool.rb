bf6fc7a61522549d49a5c610ff836d368d770f10
Christian N. Kimbi <christiankimbi@gmail.com>
2018-08-20 22:43:55 +0200

    Changed eager loading

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -2,8 +2,6 @@ module SmsTool
   account_sid = ENV['TWILIO_ACCOUNT_SID']
   auth_token = ENV['TWILIO_AUTH_TOKEN']
 
-
-
   @client = Twilio::REST::Client.new account_sid, auth_token
 
   def self.send_sms(number:,  message:)
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,5 +1,3 @@
-
-require 'sms_tool'
 namespace :notification do
   desc "Sends SMS notification to employees asking them to log if they had overtime or not"
   task sms: :environment do
