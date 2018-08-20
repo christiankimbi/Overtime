67389700900cd1f75f5b9dc14a3a8b86e581b623
Christian N. Kimbi <christiankimbi@gmail.com>
2018-08-20 06:50:22 +0200

    Added mail config settings for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address:              'mail.dren.co.za',
+      port:                 26,
+      user_name:            'info@dren.co.za',
+      password:             'mast077gild158',
+      authentication:       :login,
+      openssl_verify_mode: "none",
+      enable_starttls_auto: true }
+
+  #config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
